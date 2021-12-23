89b1bcf997642c26c5458b5cbab228a06eed5ae6
Marcx <haitianhero6@gmail.com>
2021-12-23 17:38:32 -0500

    initial commits

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+// Import ThirdWeb
+import { ThirdWeb3Provider } from '@3rdweb/hooks';
+
+// Include what chains you wanna support.
+// 4 = Rinkeby.
+const supportedChainsIds = [4];
+
+// Include what type of wallet you want to support.
+
+
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
